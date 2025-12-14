211473168f16af2bd6e6a6c9a8a4fb47027db7ec
Nikita Antropov <nikitabebradude@gmail.com>
2025-12-14 16:56:57 +0300

    Minor fixes on installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 echo "Installing dependencies"
 
 if [ -x "$(command -v pacman)" ]; then
-	sudo pacman -Sy --needed fish alacritty i3-wm dmenu i3status mate-polkit picom blueman xdg-desktop-portal xdg-desktop-portal-wlr feh ttc-iosevka j4-dmenu-desktop scrot nautilus firefox
+	sudo pacman -Sy --needed fish alacritty i3-wm dmenu i3status mate-polkit picom blueman xdg-desktop-portal xdg-desktop-portal-wlr feh ttc-iosevka j4-dmenu-desktop scrot nautilus
 elif [ -x "$(command -v apt)" ]; then
-	sudo apt update && sudo apt install -y fish alacritty i3 dmenu i3status amte-polkit picom blueman xdg-desktop-portal xdg-desktop-portal-wlr feh j4-dmenu-desktop scrot nautilus firefox
+	sudo apt update && sudo apt install -y fish alacritty i3 dmenu i3status mate-polkit picom blueman xdg-desktop-portal xdg-desktop-portal-wlr feh j4-dmenu-desktop scrot nautilus
+elif [ -x "$(command -v xbps-install)" ]; then
+	sudo xbps-install -S fish-shell alacritty i3 dmenu i3status mate-polkit picom blueman xdg-desktop-portal xdg-desktop-portal-wlr feh j4-dmenu-desktop scrot nautilus
 else
 	echo "Your distribution isn't supported, skipping dependencies installation"
 fi
